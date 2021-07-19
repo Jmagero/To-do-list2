4bb3ed6b91a0f15889335e5914097b6883942492
Jocyline Magero <jmagero2017@gmail.com>
2021-07-19 14:57:52 -0500

    Add function to iterate over the tasks array

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import './style.css';
+import '@fortawesome/fontawesome-free/js/all.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+const tasksDiv = document.getElementById('tasks');
 const tasks = [];
 class Task {
   constructor(index, description, completed) {
@@ -11,3 +15,20 @@ const task2 = new Task(2, 'second task', false);
 const task3 = new Task(3, 'third task', true);
 const task4 = new Task(4, 'forth task', true);
 tasks.push(task1, task2, task3, task4);
+const addTasks = (tasks) => {
+  for (let i = 0; i < tasks.length; i += 1) {
+    const li = document.createElement('li');
+    const task = tasks[i];
+    li.innerHTML = `
+            <div class="d-flex justify-content-between">
+                <div>
+                <input class="checkbox m-2" type="checkbox" id="${task.index}">
+                <label for="${task.index}">${task.description}</label>
+                </div>
+                <i class="fas fa-ellipsis-v"></i>
+            </div>
+            `;
+    tasksDiv.appendChild(li);
+  }
+};
+addTasks(tasks);
