910e5cc3cea888ae5120421f8c64b08c0ec4f349
Jocyline Magero <jmagero2017@gmail.com>
2021-07-21 12:43:48 -0500

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './style.css';
+
 import { Task, tasks } from './taskClass.js';
 import addTasks from './sort.js';
 import { getTasks, saveTasks } from './localStorage.js';
@@ -17,14 +17,10 @@ const inputSelector = document.querySelector('#inputTask');
 inputSelector.addEventListener('keypress', (e) => {
   if (e.key === 'Enter') {
     const task = new Task();
-    task.description = inputSelector.value
+    task.description = inputSelector.value;
     tasks.push(task);
     addTasks();
     saveTasks();
     inputSelector.value = '';
   }
 });
-
-
-
-
