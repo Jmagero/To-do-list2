e507baa88a857b5d37e8d139d378efadfaea3e4a
Jocyline Magero <jmagero2017@gmail.com>
2021-07-22 14:24:31 -0500

    Add function to enter new task

--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
 import './style.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import { Task, tasks } from './taskClass.js';
-import addTasks from './sort.js';
 import { getTasks, saveTasks } from './localStorage.js';
-import checkbox from './checkBox.js';
+import enterEvent from './enterEvent';
+import deleteTask from './delete';
 
-document.addEventListener('DOMContentLoaded', () => {
-  getTasks();
-  checkbox();
-});
 
-const inputSelector = document.querySelector('#inputTask');
+getTasks();
+enterEvent();
+deleteTask();
 
-inputSelector.addEventListener('keypress', (e) => {
-  if (e.key === 'Enter') {
-    const task = new Task();
-    task.description = inputSelector.value;
-    tasks.push(task);
-    addTasks();
-    saveTasks();
-    inputSelector.value = '';
-  }
-});
+
+
